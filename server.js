ea0587e136a0d982848ea690a170a731e0e5b68b
tiemeni <tiemanirocket@gmail.com>
2022-08-01 13:30:42 +0100

    ready to deploy 4

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const server = express();
+require('dotenv').config({path: "./.env"})
 
 server.get('/', (req, res) => {
     res.send('Hello World!, the beginning of a new story');
 })
 
 
-server.listen(5000, () => {
+server.listen(process.env.PORT || 5000, () => {
     console.log('server listen');
 })
